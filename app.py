5b42e85ef46a165f40832428e800cc7d16c86273
YC Ling <miaout17@gmail.com>
2013-08-10 16:09:32 +0800

    Commit static files

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import re
 
 DATABASE_URL = "mysql://root@127.0.0.1/news"
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='')
 app.debug = True
 
 db_url = os.getenv('DATABASE_URL')
