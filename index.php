765232eab7091f4befb5dfcc2cf464d1c5aece16
Ronny Wang <ronnywang@gmail.com>
2013-08-12 18:02:18 +0800

    add session

--- index.php
+++ index.php
@@ -3,4 +3,8 @@
 include(__DIR__ . '/webdata/init.inc.php');
 
 Pix_Controller::addCommonHelpers();
+if (!getenv('SESSION_SECRET')) {
+    die("need SESSION_SECRET");
+}
+Pix_Session::setAdapter('cookie', array('secret' => getenv('SESSION_SECRET')));
 Pix_Controller::dispatch(__DIR__ . '/webdata/');
