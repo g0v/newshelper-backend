4bcd436b21d968a45978cabff27dff17c7184633
Ronny Wang <ronnywang@gmail.com>
2013-08-19 00:39:31 +0800

    加上 ReportChangeLog->getOldDiffValues() ->getNewDiffValues()

--- webdata/models/ReportChangeLog.php
+++ webdata/models/ReportChangeLog.php
@@ -1,11 +1,41 @@
 <?php
 
+class ReportChangeLogRow extends Pix_Table_Row
+{
+    public function getOldDiffValues()
+    {
+        $old_values = json_decode($this->old_values);
+        $new_values = json_decode($this->new_values);
+        $ret = new StdClass;
+        foreach ($old_values as $k => $v) {
+            if (!property_exists($new_values, $k) or $new_values->{$k} !== $old_values->{$k}) {
+                $ret->{$k} = $v;
+            }
+        }
+        return $ret;
+    }
+
+    public function getNewDiffValues()
+    {
+        $old_values = json_decode($this->old_values);
+        $new_values = json_decode($this->new_values);
+        $ret = new StdClass;
+        foreach ($new_values as $k => $v) {
+            if (!property_exists($old_values, $k) or $new_values->{$k} !== $old_values->{$k}) {
+                $ret->{$k} = $v;
+            }
+        }
+        return $ret;
+    }
+}
+
 class ReportChangeLog extends Pix_Table
 {
     public function init()
     {
         $this->_name = 'report_change_log';
         $this->_primary = 'id';
+        $this->_rowClass = 'ReportChangeLogRow';
 
         $this->_columns['id'] = array('type' => 'int', 'auto_increment' => true);
         $this->_columns['report_id'] = array('type' => 'int');
