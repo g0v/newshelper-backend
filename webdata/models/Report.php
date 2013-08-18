bf3f8fd4b39574e35c9f8fb0e45c464b43baf581
Ronny Wang <ronnywang@gmail.com>
2013-08-18 23:36:37 +0800

    加上 Report->getDeleteReason() 印出被刪除原因

--- webdata/models/Report.php
+++ webdata/models/Report.php
@@ -1,11 +1,24 @@
 <?php
 
+class ReportRow extends Pix_Table_Row
+{
+    public function getDeleteReason()
+    {
+        if (!$this->deleted_at) {
+            return '';
+        }
+        // 刪除原因會寫在最後一次的編修記錄中的 reason
+        return json_decode(ReportChangeLog::search(array('report_id' => $this->id))->max('updated_at')->new_values)->deleted_reason;
+    }
+}
+
 class Report extends Pix_Table
 {
     public function init()
     {
         $this->_name = 'report';
         $this->_primary = 'id';
+        $this->_rowClass = 'ReportRow';
 
         $this->_columns['id'] = array('type' => 'int', 'auto_increment' => true);
         $this->_columns['created_at'] = array('type' => 'int', 'default' => 0);
