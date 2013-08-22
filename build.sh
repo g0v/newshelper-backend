44173d5ec75dc49ee361bdba146287f05081fc07
Shao-Chung Chen <dannvix@gmail.com>
2013-08-22 14:01:52 +0800

    update build script

--- build.sh
+++ build.sh
@@ -3,17 +3,17 @@
 echo "[ ] start to build application"
 
 echo "[*] installing required Node.js modules"
-npm install
+npm i
 
 echo "[*] removing old generated files"
 rm -rf _public
 
 echo "[*] building static assets"
-./node_modules/.bin/brunch build --optimize
+./node_modules/.bin/brunch b -o
 
 echo "[*] copying files into _public/"
 cp -rf _backend/* _public/
 cp -rf app/webdata/* _public/webdata/
-rm -rf app/webdata/*
+rm -rf app/webdata
 
-echo "[+] build successfully"
+echo "[+] build successfully, please deploy _public/"
