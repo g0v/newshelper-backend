0674c78269ee6ab4cdf8de37db84461590dacf53
Shao-Chung Chen <dannvix@gmail.com>
2013-08-22 14:51:04 +0800

    fix build script to deal with hidden files without duplicating files

--- build.sh
+++ build.sh
@@ -8,11 +8,13 @@ npm i
 echo "[*] removing old generated files"
 rm -rf _public
 
+echo "[*] copying backend to _public/"
+cp -rf _backend _public
+
 echo "[*] building static assets"
 ./node_modules/.bin/brunch b -o
 
-echo "[*] copying files into _public/"
-cp -rf _backend/* _backend/.* _public/
+echo "[*] copying asset files into _public/"
 cp -rf app/webdata/* _public/webdata/
 rm -rf app/webdata
 find _public/ -type f -name '*.phtml.html' -exec sh -c 'mv "$1" "${1%.phtml.html}.phtml"' _ {} \;
